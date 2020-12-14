1bded1c4d950665150f1fd10bdef16a7d2cf68c1
Brenden Dowd <brenden.dowd@yahoo.com>
2020-12-13 19:18:58 -0800

    added about and contact content

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 export default function About() {
   return (
-    <article>
-      About
-    </article>
+    <article className="backgroundFill left">
+    {/* <h3 className="left">About Me.</h3> */}
+    <p>My Name is Brenden Dowd. I'm a LA based full stack developer and designer. </p>
+    <p>I have a passion for design and problem solving. I am obsessed with the art
+          of coalescing form and function. I am skilled in JavaScript with MEAN and React, Python with Flask
+          and Django, and C# with .NET. I am a graduate of Coding Dojo and the web development course through
+          Thinkful. I am, at heart, a creator. I love the challenge and the thrill of designing something from
+          scratch and watching it come to life. I love creating intuitive applications that are just as much
+          art as tools. </p>
+          <p>I have a background as a mechanic and a filmmaker, with coding as the middle ground between the two.
+          Diagnosing and problem solving meets creation and consumption. Outside of coding I enjoy storytelling,
+          whether consuming through books, history, film and television, or video games, or telling my own stories.
+          I love learning and spend most of my free time learning new things and new skills.</p>
+  </article>
   )
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <article>
-      Contact
+    <article className="backgroundFill center">
+      <h2>For business inquiries you can reach me at{' '}<a className="mailLink" href="mailto:contact@brendendowd.com"> contact@brendendowd.com</a>.</h2>
+      <ul class="logoBar">
+        <li><a className="logoLink" href="https://linkedin.com/in/brendenrdowd"><i class="fab fa-linkedin fa-3x"></i></a></li>
+        <li><a className="logoLink" href="https://www.instagram.com/brendenrdowd/"><i class="fab fa-instagram fa-3x"></i></a></li>
+        <li><a className="logoLink" href="https://github.com/brendenrdowd"><i class="fab fa-github fa-3x"></i></a></li>
+      </ul>
     </article>
   )
 }
